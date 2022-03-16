a7e1cdbabc758a36b63ce1ff9e1e0a99eee5464e
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2022-03-15 21:43:12 -0300

    finish devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
+  has_many :articles, dependent: :destroy
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :confirmable, 
-         :database_authenticatable, 
+  devise :confirmable,
+         :database_authenticatable,
          :registerable,
-         :recoverable, 
-         :rememberable, 
+         :recoverable,
+         :rememberable,
          :trackable,
          :validatable
 end
